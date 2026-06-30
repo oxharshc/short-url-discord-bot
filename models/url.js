3690818abb18958d114fabf65217ab0a798947d0
Harshit Chauhan <98049847+oxharshc@users.noreply.github.com>
2026-06-30 23:35:01 +0530

    add: schema for url

---
+++ models/url.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    clicks: {
+      type: Number,
+      default: 0,
+    },
+    createdBy: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+module.exports = mongoose.model("Url", urlSchema);
